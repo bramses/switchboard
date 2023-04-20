d4a1a0c77f6f4e5a82170c1e0b2c1933a381bf38
Bram Adams <3282661+bramses@users.noreply.github.com>
2023-04-19 22:32:00 -0400

    added example key in swagger

--- auth.py
+++ auth.py
@@ -3,7 +3,7 @@ from fastapi import Security, HTTPException, Depends
 from starlette.status import HTTP_401_UNAUTHORIZED
 from config import Settings, get_settings
 
-api_key_header = APIKeyHeader(name="Authorization", auto_error=False, description="API Key Header")  
+api_key_header = APIKeyHeader(name="Authorization", auto_error=False, description="API Key Header (example is 'my file key')")  
 
 
 async def get_api_key(settings: Settings = Depends(get_settings), api_key_header: str = Security(api_key_header)):
