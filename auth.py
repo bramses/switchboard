dab9031a8b20ecbe58d8c8346f7f28f68f03bbb1
Bram Adams <3282661+bramses@users.noreply.github.com>
2023-04-19 22:22:48 -0400

    cors auth and more!

--- auth.py
+++ auth.py
@@ -1,14 +1,17 @@
 from fastapi.security.api_key import APIKeyHeader
 from fastapi import Security, HTTPException, Depends
-from starlette.status import HTTP_403_FORBIDDEN
+from starlette.status import HTTP_401_UNAUTHORIZED
 from config import Settings, get_settings
 
-api_key_header = APIKeyHeader(name="access_token", auto_error=False)
+api_key_header = APIKeyHeader(name="Authorization", auto_error=False, description="API Key Header")  
+
 
 async def get_api_key(settings: Settings = Depends(get_settings), api_key_header: str = Security(api_key_header)):
+    if "Bearer" in api_key_header:
+        api_key_header = api_key_header.replace("Bearer ", "")
     if api_key_header == settings.API_KEY:
         return api_key_header   
     else:
         raise HTTPException(
-            status_code=HTTP_403_FORBIDDEN, detail="Could not validate API KEY"
+            status_code=HTTP_401_UNAUTHORIZED, detail="Could not validate API KEY"
         )
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from fastapi import Depends, FastAPI, Security
+from fastapi import Depends, FastAPI, Response
 import auth
 from fastapi.security.api_key import APIKey
-
+from fastapi.responses import HTMLResponse
 from fastapi.openapi.utils import get_openapi
-import json
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+    "https://chat.openai.com",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/secure")
 async def info(api_key: APIKey = Depends(auth.get_api_key)):
@@ -13,22 +28,71 @@ async def info(api_key: APIKey = Depends(auth.get_api_key)):
         "default variable": api_key
     }
 
-@app.get("/") 
-async def main_route():     
-  return {"message": "switchboard"}
+@app.get("/todo-list", description="Get a list of todos", tags=["todos"])
+async def todo_list(api_key: APIKey = Depends(auth.get_api_key)):
+    print(api_key)
+    todos = [
+        {
+            "id": 1,
+            "title": "Todo 1",
+            "description": "Todo 1 description"
+        },
+        {
+            "id": 2,
+            "title": "Todo 2",
+            "description": "Todo 2 description"
+        }
+    ]
+    
+    return {
+        "todos": todos
+    }
+
+
+@app.get("/", description="homepage list of routes", tags=["homepage"]) 
+async def main_route():   
+  html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI on Vercel</title>
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+    </head>
+    <body>
+        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+            <h1>switchboard</h1>
+            <ul>
+                <li><a href="/docs">/docs</a></li>
+                <li><a href="/redoc">/redoc</a></li>
+                <li><a href="/openapi.json">/openapi.json</a></li>
+                <li><a href="/.well-known/ai-plugin.json">/.well-known/ai-plugin.json</a></li>
+            </ul>
+            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
+        </div>
+    </body>
+</html>
+"""  
+  return HTMLResponse(html)
 
 def custom_openapi():
     if app.openapi_schema:
         return app.openapi_schema
     openapi_schema = get_openapi(
-        title="Custom title",
-        version="2.5.0",
-        description="This is a very custom OpenAPI schema",
+        title="switchboard",
+        version="3.0.2",
+        description="description for switchboard api",
         routes=app.routes,
     )
     openapi_schema["info"]["x-logo"] = {
         "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
     }
+    # if components.schema is not defined add it (for openai error: Error getting system message: {"message":"Could not parse OpenAPI spec for plugin: ['In components section, schemas subsection is not an object']"})
+    if "components" not in openapi_schema:
+        openapi_schema["components"] = {}
+        openapi_schema["components"]["schemas"] = {}
+    else:
+        if "schemas" not in openapi_schema["components"]:
+            openapi_schema["components"]["schemas"] = {}
     app.openapi_schema = openapi_schema
     return app.openapi_schema
 
@@ -36,6 +100,6 @@ def custom_openapi():
 @app.get("/.well-known/ai-plugin.json")
 async def plugin_manifest():
     file = open("./.well-known/ai-plugin.json", "r")
-    return json.load(file)
+    return Response(content=file.read(), media_type="application/json")
 
 app.openapi = custom_openapi
